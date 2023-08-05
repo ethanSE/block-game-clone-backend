1ce8a1231a7a4e404492e19de749baefb4f46166
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2023-08-05 12:39:48 -0700

    improve build automation

--- wasm_build_automation/add_types.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-const path = "pkg/package.json"
-const pkg = JSON.parse(fs.readFileSync(path))
-pkg.files = pkg.files.concat("types") 
-fs.writeFileSync("pkg/package.json", JSON.stringify(pkg, null, 2));
