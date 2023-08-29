53224897abe790b3be9f883512b1256ed8366dbb
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2023-08-29 14:35:26 -0700

    use hashmaps instead of rigid structs for players, pieces, score

--- block-game-clone-backend/src/lib.rs
+++ block-game-clone-backend/src/lib.rs
@@ -2,38 +2,9 @@ mod board;
 mod board_state;
 mod game_mode;
 mod game_state;
-mod hand;
 mod piece;
 mod player;
 mod player_hand_state;
 mod player_state;
 mod ts_interop;
 mod utils;
-
-use game_state::GameState;
-use ts_interop::Action;
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn new_game(game_mode_str: &str) -> String {
-    let game_mode = serde_json::from_str::<game_mode::GameMode>(game_mode_str);
-
-    match game_mode {
-        Ok(gs) => serde_json::to_string(&GameState::new(gs)).unwrap(),
-        _ => "Error".to_string(),
-    }
-}
-
-#[wasm_bindgen]
-pub fn next_game_state(current_state_s: &str, action_s: &str) -> String {
-    let current_state = serde_json::from_str::<game_state::GameState>(current_state_s);
-    let action = serde_json::from_str::<Action>(action_s);
-
-    match (current_state, action) {
-        (Ok(mut cs), Ok(action)) => {
-            cs.apply_action(action);
-            serde_json::to_string(&cs).unwrap()
-        }
-        _ => "invalid".to_string(),
-    }
-}
--- block-game-clone-backend/src/ts_interop.rs
+++ block-game-clone-backend/src/ts_interop.rs
@@ -1,10 +1,37 @@
-use std::ops::Index;
-
 use nalgebra::Vector3;
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
+use wasm_bindgen::prelude::wasm_bindgen;
 
-use crate::{piece::PieceName, player::Player};
+#[wasm_bindgen]
+pub fn new_game(game_mode_str: &str) -> String {
+    let game_mode = serde_json::from_str::<game_mode::GameMode>(game_mode_str);
+
+    match game_mode {
+        Ok(gs) => serde_json::to_string(&GameState::new(gs)).unwrap(),
+        _ => "Error".to_string(),
+    }
+}
+
+#[wasm_bindgen]
+pub fn next_game_state(current_state_s: &str, action_s: &str) -> String {
+    let current_state = serde_json::from_str::<game_state::GameState>(current_state_s);
+    let action = serde_json::from_str::<Action>(action_s);
+
+    match (current_state, action) {
+        (Ok(mut cs), Ok(action)) => {
+            cs.apply_action(action);
+            serde_json::to_string(&cs).unwrap()
+        }
+        _ => "invalid".to_string(),
+    }
+}
+
+use crate::{
+    game_mode,
+    game_state::{self, GameState},
+    piece::PieceName,
+};
 
 #[derive(Serialize, Deserialize, TS)]
 #[ts(export, export_to = "pkg/types/V3.ts")]
@@ -30,21 +57,4 @@ pub enum Action {
 pub enum RotationAxis {
     X,
     Y,
-}
-
-#[derive(Serialize, Deserialize, TS, Default, Clone, Debug)]
-#[ts(export, export_to = "pkg/types/Score.ts")]
-pub struct Score {
-    pub p1: i8,
-    pub p2: i8,
-}
-
-impl Index<Player> for Score {
-    type Output = i8;
-    fn index(&self, player: Player) -> &Self::Output {
-        match player {
-            Player::P1 => &self.p1,
-            Player::P2 => &self.p2,
-        }
-    }
 }
